08b2cc28d7215c2c853e8cacf43d0db53c56cf68
hamitseyrek <hamitseyrek@gmail.com>
2022-01-20 09:21:13 +0300

    it worked

--- FaceRecognitionTutorial/ViewController.swift
+++ FaceRecognitionTutorial/ViewController.swift
@@ -6,14 +6,34 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func signInClicked(_ sender: Any) {
+        print("tgere")
+        let authContext = LAContext()
+        var error : NSError?
+        if authContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            authContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "is it you?") { success, error in
+                if success == true {
+                    // perform segue must be run in main thread
+                    DispatchQueue.main.async {
+                        self.performSegue(withIdentifier: "toSecondVC", sender: nil)
+                    }
+                }else {
+                    self.myLabel.text = "Error!"
+                }
+            }
+        }
+    }
+    
 }
 
+            
